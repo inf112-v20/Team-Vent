3054d5003dc33e7cfafebf713c8ddb14d7ab91ad
odainanna <oda.stene@student.uib.no>
2020-02-04 19:09:19 +0100

    Revert "Klasser og grensesnitt for brettet"

--- src/main/java/inf112/skeleton/app/board/Direction.java
+++
@@ -1,5 +0,0 @@
-package inf112.skeleton.app.board;
-
-public enum Direction {
-    NORTH, SOUTH, EAST, WEST
-}
--- src/main/java/inf112/skeleton/app/board/IConveyor.java
+++
@@ -1,7 +0,0 @@
-package inf112.skeleton.app.board;
-
-public interface IConveyor extends ITile {
-    boolean isExpress();
-
-    Direction getDirection();
-}
