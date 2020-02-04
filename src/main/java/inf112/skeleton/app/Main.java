82c7c1c8a35c891804dcb5aeef6772090cba0dd3
odainanna <odainanna@gmail.com>
2020-02-04 23:30:28 +0100

    Refaktorering

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -7,10 +7,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "HelloRobot";
+        cfg.title = "RoboRally";
         cfg.width = 600;
         cfg.height = 600;
-
         new LwjglApplication(new HelloRobot(), cfg);
     }
 }
