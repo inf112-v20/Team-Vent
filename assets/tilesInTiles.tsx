1406ee8e7e8d3293405497528b4a26000e2222b8
Marius A. Pedersen <vim008@uib.no>
2020-02-24 12:39:56 +0100

    Added cool-robot from link, (animation will come later)

---
+++ assets/tilesInTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="tiles" tilewidth="300" tileheight="300" tilecount="4" columns="2">
+ <image source="assets/tiles.png" width="720" height="720"/>
+</tileset>
