b8586bd4fc52694a8c934f67243daacf9e9cabb1
odainanna <odainanna@gmail.com>
2020-03-01 00:11:19 +0100

    Make Codacy happy

--- src/main/java/inf112/skeleton/app/Constants.java
+++ src/main/java/inf112/skeleton/app/Constants.java
@@ -1,8 +1,6 @@
 package inf112.skeleton.app;
 
 public final class Constants {
-    private Constants() { // do not instantiate
-    }
 
     // Names of the tile layers in the tiled map
     public static final String WALL_LAYER = "Wall";
@@ -10,4 +8,7 @@ public final class Constants {
     public static final String TILE_LAYER = "Tile";
     public static final String LASER_LAYER = "Laser";
     public static final String FLAG_LAYER = "Flag";
+
+    private Constants() { // do not instantiate
+    }
 }
