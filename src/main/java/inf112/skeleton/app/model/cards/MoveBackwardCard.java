1c1c5637d899641344e3349410661030895d8fcc
Marius A. Pedersen <vim008@uib.no>
2020-03-09 16:11:58 +0100

    Deck of cards and backwards

---
+++ src/main/java/inf112/skeleton/app/model/cards/MoveBackwardCard.java
@@ -0,0 +1,19 @@
+package inf112.skeleton.app.model.cards;
+
+import inf112.skeleton.app.model.board.Location;
+
+public class MoveBackwardCard implements IProgramCard {
+
+    public Location instruction(Location loc) {
+        return loc.backward();
+    }
+
+    @Override
+    public String toString() {
+        return "MOVE Backwards";
+    }
+
+    public int value() {
+    return 2;
+    }
+}
