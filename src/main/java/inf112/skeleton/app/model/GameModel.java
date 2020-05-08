82c9a628598155c22613c3dd0a93671aa111a57e
JohnIsak <John.Villanger@student.uib.no>
2020-05-08 14:29:57 +0200

    hole gamestate fix

--- src/main/java/inf112/skeleton/app/model/GameModel.java
+++ src/main/java/inf112/skeleton/app/model/GameModel.java
@@ -251,8 +251,8 @@ public class GameModel {
             case MOVE_TWO:
                 if (robotState.getDead()) return;
                 doMovement(phaseNumber, initialState, robotState, cardSteps, robotState.getLocation().getDirection());
-                doBordersAndHoles(initialState);
                 initialState = updateLastState(initialState, cardSteps.get(phaseNumber));
+                doBordersAndHoles(initialState);
                 robotState = initialState.getState(robotState.getRobot());
                 // no break
             case MOVE_ONE:
